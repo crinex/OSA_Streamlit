c15c3934711520557bca3b4abf1aff81c52e6de2
unknown <crinexk@gmail.com>
2022-12-20 12:51:50 +0900

    'final_code_change_1'

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import StandardScaler
 import shap
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.write("""
@@ -20,11 +21,9 @@ X['ESS'] = X['ESS'].fillna(X['ESS'].mean())
 Y = X['RDI']
 X.drop('RDI', axis=1, inplace=True)
 X.drop('Unnamed: 0', axis=1, inplace=True)
-# st.write(X.head())
-# st.title('Predict your Sleep Apnea Score')
-# st.markdown('Toy model to predict your Sleep Apnea Score')
+
 st.sidebar.header('Specify Input Parameters')
-# X.Ht.min(), X.Ht.max(), X.Ht.mean()
+reg = st.sidebar.selectbox('Choose one Regression Model', ('MLP', 'RF'))
 def user_input_features():
     HT = st.sidebar.slider('Height', 1.20, 2.00, 1.50)
     WT = st.sidebar.slider('Weight', 30., 180., 70.)
@@ -48,13 +47,21 @@ def user_input_features():
 df = user_input_features()
 st.header('Specified Input parameters')
 st.write(df)
-# st.write('Your BMI is ', df['BMI'])
 st.write('---')
 df['Sex'] = df['Sex'].replace({'F':0, 'M':1})
-
 X['Sex'] = X['Sex'].replace({'F':0, 'M':1})
-model = RandomForestRegressor()
-model.fit(X,Y)
+
+if reg == 'MLP':
+    model = MLPRegressor()
+    model.fit(X,Y)
+    explainer = shap.KernelExplainer(model.predict, df)
+    shap_values = explainer.shap_values(X)
+elif reg == 'RF':
+    model = RandomForestRegressor()
+    model.fit(X,Y)
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X)
+
 
 prediction = model.predict(df)
 
@@ -73,9 +80,6 @@ st.write(prediction)
 st.write(f"### Your OSA Grade is ***{grade}***")
 st.write('---')
 
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
-
 st.header('Feature Importance')
 plt.title('Feature importance based on SHAP values')
 shap.summary_plot(shap_values, X)
@@ -85,54 +89,3 @@ st.write('---')
 plt.title('Feature importance based on SHAP values (Bar)')
 shap.summary_plot(shap_values, X, plot_type="bar")
 st.pyplot(bbox_inches='tight')
-# df['SEX'] = 
-# st.header("Sleep Apnea Features")
-# col1, col2, col3 = st.columns(3)
-
-# sc = StandardScaler()
-# result = 0
-
-# def predict(data):
-#     clf = load_model("osa_reg2.h5")
-#     return clf.predict(data)
-
-# with col1:
-#     # st.text('Height (m)')
-#     height = st.number_input('Your Height(m), honestly')
-
-#     sex = st.selectbox(
-#         'Choose your Sex (Male / Female)',
-#         ('Male', 'Female')
-#     )
-#     if sex is 'Male':
-#         sex = 0
-#     else:
-#         sex = 1
-
-#     plmi = st.number_input('PLMI Score')
-
-# with col2:
-#     # st.text('Weight (kg)')
-#     weight = st.number_input('Your Weight(kg), honestly')
-
-#     ess = st.number_input('ESS Score')
-#     age = st.number_input('Your Age')
-#     bmi = weight / (height*height)
-#     st.write('Your BMI is ', bmi)
-
-# with col3:
-#     if st.button('Predict'):
-#         data = np.array([age, sex, height, weight, bmi, plmi, ess])
-#         data = data.reshape(1, -1)
-#         data  = sc.fit_transform(data)
-#         result = predict(data)
-        # result = predict(
-        # np.array([[age, sex, height, weight, bmi, plmi, ess]]))
-    # st.text(result)
-    # height = st.number_input('Your Height, honestly')
-
-# st.text('')
-# if st.button("Predict type of Iris"):
-#     result = predict(
-#         np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
-#     st.text(result[0])
